273b672a9a6b445b909c7879ceff4f46c569800c
harrisanvo <harris@anvo.com>
2017-11-29 17:45:42 +0800

    优化代码

--- adminhoutai/index.php
+++ adminhoutai/index.php
@@ -1,3 +1,8 @@
 <?php
-include 'admin778899.php';
-?>
+function load_file($name, $path = __DIR__)
+{
+    $class_file = $path . DIRECTORY_SEPARATOR . $name . '.php';
+    require_once $class_file;
+}
+load_file('admin778899');
+//include 'admin778899.php';
